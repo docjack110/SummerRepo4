e46c372046d4bfa93b9406173dae2d8e14950229
docjack110 <123031073+docjack110@users.noreply.github.com>
2023-03-29 19:29:10 -0400

    Update DS413613ClassworkLabForkingRfile.R

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -2,14 +2,13 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 mpg
-# My name is John Doe
-# My favorite color is blue
-# This is my coding that generates Boxplots for transmission types
+# My name is Kimbria Jackson
+# This is data source is the mpg data table
 ggplot(data = mpg) +
   geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
   ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
   xlab("transmission types") +
   ylab("displacement") 
-  
+  coord_flip()
 
 
